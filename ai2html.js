5a1df4193fe23974c46e49488b0b79a9b2951b8e
Gregor Aisch <gregor.aisch@nytimes.com>
2015-11-19 17:02:34 -0500

    fixed artboard bounds check on svg exports

--- ai2html.js
+++ ai2html.js
@@ -1774,8 +1774,9 @@ if (doc.documentColorSpace!="DocumentColorSpace.RGB") {
 
 			// if in svg export, hide path elements outside of the current artboard
 			if (docSettings.image_format[0] == 'svg') {
+
 				// save refs to elements we are hiding 
-				var hiddenItemsOutsideArtboard = hideElementsOutsideArtboardRect(activeArtboard.artboardRect);
+				var hiddenItemsOutsideArtboard = hideElementsOutsideArtboardRect(activeArtboardRect);
 			}
 
 			exportImageFiles(imageDestination,abW,abH,docSettings.image_format,1.0,docSettings.use_2x_images_if_possible);
@@ -2125,52 +2126,60 @@ function addDot(left, top, radius, color) {
 
 
 function hideElementsOutsideArtboardRect(artbnds) {
-	var hidden = [];
-	for (var lid=0; lid<doc.layers.length; lid++) {
-		var layer = doc.layers[lid];
-		if (layer.visible) { // ignore invisible layers
-			var checkItemGroups = [layer.pathItems, layer.symbolItems, layer.compoundPathItems];
-			var groups = getGroups([], layer.groupItems);
-			for (var g=0; g<groups.length; g++) {
-				checkItemGroups.push(groups[g].pathItems);
-				checkItemGroups.push(groups[g].symbolItems);
-				checkItemGroups.push(groups[g].compoundPathItems);
-			}
-			for (var cig=0; cig<checkItemGroups.length; cig++) {
-				for (var item_i=0; item_i<checkItemGroups[cig].length; item_i++) {
-					var check_item = checkItemGroups[cig][item_i],
-						item_bnds = check_item.visibleBounds;
-					// bounds are [left,-top,right,-bottom]
-					if (item_bnds[0] > artbnds[2] ||
-						item_bnds[2] < artbnds[0] ||
-						item_bnds[1] < artbnds[3] ||
-						item_bnds[3] > artbnds[1]) {
-						if (!check_item.hidden) {
-							hidden.push(check_item);
-							check_item.hidden = true;	
+	var hidden = [], all_groups;
+	checkLayers(doc.layers);
+
+	function checkLayers(layers) {
+		for (var lid=0; lid<layers.length; lid++) {
+			var layer = layers[lid];
+			if (layer.visible) { // only deal with visible layers
+				var checkItemGroups = [layer.pathItems, layer.symbolItems, layer.compoundPathItems];
+				all_groups = [];
+				traverseGroups(layer);
+				// feedback.push('layer.groupItems '+layer.groupItems.length);
+				// alert('groups: '+groups.length);
+				for (var g=0; g<all_groups.length; g++) {
+					checkItemGroups.push(all_groups[g].pathItems);
+					checkItemGroups.push(all_groups[g].symbolItems);
+					checkItemGroups.push(all_groups[g].compoundPathItems);
+				}
+				for (var cig=0; cig<checkItemGroups.length; cig++) {
+					for (var item_i=0; item_i<checkItemGroups[cig].length; item_i++) {
+						var check_item = checkItemGroups[cig][item_i],
+							item_bnds = check_item.visibleBounds;
+						// bounds are [left,-top,right,-bottom]
+						if (item_bnds[0] > artbnds[2] ||
+							item_bnds[2] < artbnds[0] ||
+							item_bnds[1] < artbnds[3] ||
+							item_bnds[3] > artbnds[1]) {
+							if (!check_item.hidden) {
+								hidden.push(check_item);
+								check_item.hidden = true;	
+							}
 						}
 					}
+				}
+				if (layer.layers.length > 0) checkLayers(layer.layers);
+			}
+		}
+		function traverseGroups(groupItems) {
+			for (var g=0;g<groupItems.length;g++) {
+				// check group bounds
+				var bnds = groupItems[g].visibleBounds;
+				if (bnds[0] > artbnds[2] || bnds[2] < artbnds[0] || bnds[1] < artbnds[3] || bnds[3] > artbnds[1]) {
+					// group entirely out of artboard, so ignore
+					groupItems[g].hidden = true;
+					hidden.push(groupItems[g]);
+				} else {
+					// recursively check sub-groups
+					all_groups.push(groupItems[g]);
+					if (groupItems[g].groupItems.length > 0) {
+						getGroups(groupItems[g].groupItems);
+					}	
 				}
 			}
 		}
 	}
-	function getGroups(res, groupItems) {
-		for (var g=0;g<groupItems.length;g++) {
-			// check group bounds
-			var bnds = groupItems[g].visibleBounds;
-			if (bnds[0] > artbnds[2] || bnds[2] < artbnds[0] || bnds[1] < artbnds[3] || bnds[3] > artbnds[1]) {
-				// group entirely out of artboard, so ignore
-				groupItems[g].hidden = true;
-				hidden.push(groupItems[g]);
-			} else {
-				// recursively check sub-groups
-				res.push(groupItems[g]);
-				if (groupItems[g].groupItems.length > 0) {
-					getGroups(res, groupItems[g].groupItems);
-				}	
-			}
-		}
-		return res;
-	}
+
 	return hidden;
 }
