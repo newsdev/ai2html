393d21a57272957cf77a4a78458c49ad22ab0654
Matthew Bloch <masiyou@gmail.com>
2017-03-12 21:16:25 -0400

    Minor code reorganization

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,16 +1,22 @@
-var lib = require('../').internal;
+var lib = require('../');
 var assert = require('assert');
 
 describe('Utility function tests', function() {
+
+  describe('testBoundsIntersection()', function() {
+    it('true if overlapping bounds', function() {
+      var a = [],
+          b = []
+      assert.equal(lib.testBoundsIntersection(a, b));
+    });
+  });
+
 
   describe('trim()', function() {
     it('removes spaces from beginning and end of strings', function() {
       assert.equal(lib.trim('  \t\rfoo \n'), 'foo');
     });
   });
-
-
-
 
 
 });
