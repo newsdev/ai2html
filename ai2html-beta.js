bcc59fdcb4c22377dfce1b9e4590c708d8c9bfb5
Matthew Bloch <masiyou@gmail.com>
2017-04-10 13:58:28 -0400

    Improve placement of rotated area text

--- ai2html-beta.js
+++ ai2html-beta.js
@@ -2089,8 +2089,25 @@ function formatCssPct(part, whole) {
 
 function getUntransformedTextBounds(textFrame) {
   var copy = textFrame.duplicate(textFrame.parent, ElementPlacement.PLACEATEND);
-  copy.transform(clearMatrixShift(app.invertMatrix(textFrame.matrix)));
+  var matrix = clearMatrixShift(textFrame.matrix);
+  copy.transform(app.invertMatrix(matrix));
   var bnds = copy.geometricBounds;
+  if (textFrame.kind == TextType.AREATEXT) {
+    // prevent offcenter problem caused by extra vertical space in text area
+    // TODO: de-kludge
+    // this would be much simpler if <TextFrameItem>.convertAreaObjectToPointObject()
+    // worked correctly (throws MRAP error when trying to remove a converted object)
+    var trueWidth = (bnds[2] - bnds[0]);
+    copy.transform(matrix);
+    // Transforming outlines avoids the offcenter problem, but width of bounding
+    // box needs to be set to width of transformed TextFrame for correct wrapping
+    copy = copy.createOutline();
+    copy.transform(app.invertMatrix(matrix));
+    bnds = copy.geometricBounds;
+    var dx = Math.ceil(trueWidth - (bnds[2] - bnds[0])) / 2;
+    bnds[0] -= dx;
+    bnds[2] += dx;
+  }
   copy.remove();
   return bnds;
 }
