a4032c0a71c125f20679400cfc213df74a45ad1d
Matthew Bloch <masiyou@gmail.com>
2017-05-17 14:55:41 -0400

    Real fix for runtime error during SVG export

--- ai2html-beta.js
+++ ai2html-beta.js
@@ -2583,8 +2583,8 @@ function copyArtboardForImageExport(ab, masks) {
     // only remove text frames, for performance
     // TODO: consider checking all item types
     // TODO: consider checking subgroups (recursively)
-    // PROBLEM: causes runtime error "Runtime error on line 662: No such element"
-    // forEach(group.textFrames, removeItemIfHidden);
+    // FIX: convert group.textFrames to array to avoid runtime error "No such element" in forEach()
+    forEach(toArray(group.textFrames), removeItemIfHidden);
   }
 
   function removeItemIfHidden(item) {
